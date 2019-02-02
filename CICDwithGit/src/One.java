ee714bed5719121d8bfcf6dcde00e2fc994210ab
selenium75 <shanthuslm@gmail.com>
2019-02-02 11:27:23 +0530

    modifiedvdata

--- CICDwithGit/src/One.java
+++ CICDwithGit/src/One.java
@@ -4,7 +4,7 @@ public class One {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("selenium automation");
+		System.out.println("Jmetrrt automation");
 	}
 
 }
